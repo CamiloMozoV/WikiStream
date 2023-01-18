2e852c2f000596d36ffd4f829cfd6389542a6d4c
Camilo Mozo <camilomozov@gmail.com>
2023-01-18 10:27:38 -0500

    refactor: change bootstrap server name argument in KafkaProducer class

--- services/kafkaWikiStream/src/WikiStream.py
+++ services/kafkaWikiStream/src/WikiStream.py
@@ -4,7 +4,7 @@ from kafka import KafkaProducer
 from sseclient import SSEClient as EventSource
 
 kafka_producer = KafkaProducer(
-    bootstrap_servers=["spark-kafka:9092"],
+    bootstrap_servers=["kafka:9092"],
     api_version=(2,0,2),
     value_serializer=lambda x: json.dumps(x).encode("utf-8")
 )
