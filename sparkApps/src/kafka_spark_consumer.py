c46c039014693c0593c74ac49d3bd33ea96a5f1f
Camilo Mozo <camilomozov@gmail.com>
2023-01-19 22:29:30 -0500

    feat: stream data schema

--- sparkApps/src/kafka_spark_consumer.py
+++ sparkApps/src/kafka_spark_consumer.py
@@ -1,6 +1,14 @@
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.types import (
-    StringType
+    StringType, 
+    StructField,
+    StructType,
+    BooleanType,
+    IntegerType,
+    LongType
+)
+from pyspark.sql.functions import (
+    from_json
 )
 
 def read_kafka_stream(spark_session: SparkSession) -> DataFrame:
@@ -16,8 +24,55 @@ def read_kafka_stream(spark_session: SparkSession) -> DataFrame:
     
     return df_raw
 
+def data_schema_stream(df: DataFrame) -> DataFrame:
+    # Event data Schema
+    schema = StructType([
+        StructField("$schema", StringType(), True),
+        StructField("bot", BooleanType(), True),
+        StructField("comment", StringType(), True),
+        StructField("id", StringType(), True),
+        StructField("length", StructType([ 
+                                            StructField("new", IntegerType(), True),
+                                            StructField("old", IntegerType(), True)
+                                        ]), True),
+        StructField("meta", StructType([
+                                            StructField("domain", StringType(), True),
+                                            StructField("dt", StringType(), True),
+                                            StructField("id", StringType(), True),
+                                            StructField("offset", LongType(), True),
+                                            StructField("partition", LongType(), True),
+                                            StructField("request_id", StringType(), True),
+                                            StructField("stream", StringType(), True),
+                                            StructField("topic", StringType(), True),
+                                            StructField("uri", StringType(), True),
+                                       ]), True),
+        StructField("minor", BooleanType(), True),
+        StructField("namespace", IntegerType(), True),
+        StructField("parsedcomment", StringType(), True),
+        StructField("patrolled", BooleanType(), True),
+        StructField("revision", StructType([
+                                            StructField("new", IntegerType(), True),
+                                            StructField("old", IntegerType(), True)
+                                        ]), True),
+        StructField("server_name", StringType(), True),
+        StructField("server_script_path", StringType(), True),
+        StructField("server_url", StringType(), True),
+        StructField("timestamp", StringType(), True),
+        StructField("title", StringType(), True),
+        StructField("type", StringType(), True),
+        StructField("user", StringType(), True),
+        StructField("wiki", StringType(), True)
+    ])
+    
+    # Create DataFrame setting schema for event data
+    df_wikiStream = df.withColumn("value", from_json("value", schema))
+    return df_wikiStream
+
 def kafka_spark_consumer(spark_session: SparkSession) -> None:
-    read_kafka_stream(spark_session)
+    df_raw = read_kafka_stream(spark_session)
+    df_wikiStream = data_schema_stream(df_raw)
+    
+    
 
 if __name__=="__main__":
     # Create a session
